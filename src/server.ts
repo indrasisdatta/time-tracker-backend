e06f70a3876fd68d33e59d8b4f7c36614b4c7cdf
Indrasis Datta <indrasis.datta@yahoo.co.in>
2023-09-04 18:46:12 +0530

    Vercel build, start scripts

--- src/server.ts
+++ src/server.ts
@@ -29,6 +29,10 @@ mongoose.connection.on("disconnected", (res) => {
 });
 /* DB connection code ends */
 
+app.get("/", (req: Request, res: Response) => {
+  return res.send("App is running");
+});
+
 app.use("/", routes);
 
 // Custom error handling middleware for 500 errors
