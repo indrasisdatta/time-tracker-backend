9f801b33c89fcaab963e246afb1ba17153f127f9
Indrasis Datta <indrasis.datta@yahoo.co.in>
2023-09-20 18:37:17 +0530

    CORS update to support local IP address

--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,20 @@ dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 const app: Express = express();
 
+let corsConfig = {};
+if (process.env.NODE_ENV === "local") {
+  corsConfig = {
+    origin: [
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
+      "http://192.168.1.6:3000",
+    ],
+  };
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(corsConfig));
 
 /* Swagger API */
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
