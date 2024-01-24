81bfa6cf8b375088c59a32d45f6a249db53773c3
Indrasis Datta <indrasis.datta@yahoo.co.in>
2024-01-24 12:20:50 +0530

    Health check API

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,21 @@ import { Router } from "express";
 import categoryRoutes from "./categoryRoutes";
 import timesheetRoutes from "./timesheetRoutes";
 import { NextFunction, Request, Response } from "express";
+import { API_STATUS } from "../config/constants";
 
 const router = Router();
 
 router.use("/category", categoryRoutes);
 router.use("/timesheet", timesheetRoutes);
 
-// Custom 404 error handler
+/* Health check URL */
+router.get("/health", (req: Request, res: Response) => {
+  res
+    .status(200)
+    .json({ status: API_STATUS.SUCCESS, data: "Server is running" });
+});
+
+/* Custom 404 error handler */
 router.use((req: Request, res: Response, next: NextFunction) => {
   res.status(404).json({ status: 0, message: "Not Found" });
 });
